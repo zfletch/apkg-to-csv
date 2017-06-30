9df133c97e25193ce94407366082428e1f4b02db
zfletch <zfletch2@gmail.com>
2017-06-29 23:56:06 -0400

    uzip file

--- bin/apkg_to_csv.rb
+++ bin/apkg_to_csv.rb
@@ -1,15 +1,34 @@
 #!/usr/bin/env ruby
 
 require 'sqlite3'
+require 'zip'
 require './lib/apkg_to_csv'
+require 'tempfile'
 
 filename = ARGV[0]
 
 if !filename
-  puts "Filename requires"
+  puts 'Filename requires'
   exit 1
 end
 
-db = SQLite3::Database.new(filename)
+file_written = false
+tempfile = Tempfile.new('db')
+
+Zip::File.open(filename) do |zip_file|
+  zip_file.each do |entry|
+    if entry.name == 'collection.anki2'
+      tempfile.write(entry.get_input_stream.read)
+      file_written = true
+    end
+  end
+end
+
+if !file_written
+  puts 'Could not extract zip file correctly'
+  exit 1
+end
+
+db = SQLite3::Database.new(tempfile.path)
 
 puts ApkgToCsv.csv(db).csv
