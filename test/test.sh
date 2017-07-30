a2213d120bff41e98ad5717e1edca6540eb532a6
zfletch <zfletch2@gmail.com>
2017-07-29 23:32:42 -0400

    travis support

--- test/test.sh
+++ test/test.sh
@@ -2,4 +2,13 @@
 
 ./bin/apkg_to_csv.rb test/test.apkg > test/__compare_test.csv
 
-cmp --silent test/test.csv test/__compare_test.csv && echo "Tests passed" && rm test/__compare_test.csv || echo "Tests failed: Files not equal"
+cmp --silent test/test.csv test/__compare_test.csv || STATUS=$?
+
+if [[ $STATUS -eq 0 ]]; then
+  echo "Tests passed"
+  rm test/__compare_test.csv
+else
+  echo "Tests failed: Files not equal"
+fi
+
+exit $STATUS
